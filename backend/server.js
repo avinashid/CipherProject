dd8c714a33768b19d3d04180f56f5c63bbbedfef
Avinash Kumar <avinashkumar532004@gmail.com>
2023-04-19 01:39:53 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 connectDB();
 const app = express();
 const corsOptions = {
-  origin: "http://127.0.0.1:5173",
+  origin: "https://avinashid.github.io/CipherFront",
 };
 
 app.use(express.json());
